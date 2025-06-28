8cd65682eaacfd06f6643dd31f06d38156c5abde
Nemanja Zbiljić <nemanja.zbiljic@gmail.com>
2025-06-28 21:55:33 +0200

    build: add MediaMTX OpenAPI spec download

---
+++ tools/mediamtx.mk
@@ -0,0 +1,17 @@
+# renovate: datasource=github-releases depName=bluenviron/mediamtx
+MEDIAMTX_VERSION ?= v1.12.3
+
+tmp/mediamtx: tmp
+	mkdir -p $@
+
+tmp/mediamtx/$(MEDIAMTX_VERSION): tmp/mediamtx
+	mkdir -p $@
+
+tmp/mediamtx/$(MEDIAMTX_VERSION)/openapi.yaml: tmp/mediamtx/$(MEDIAMTX_VERSION)
+	echo "Downloading MediaMTX '$(MEDIAMTX_VERSION)' openapi.yaml"
+	curl -sSL \
+		https://github.com/bluenviron/mediamtx/raw/refs/tags/$(MEDIAMTX_VERSION)/apidocs/openapi.yaml \
+		--output $(PROJECT_ROOT)/$@
+
+.PHONY: tmp/mediamtx/openapi.yaml
+tmp/mediamtx/openapi.yaml: tmp/mediamtx/$(MEDIAMTX_VERSION)/openapi.yaml
--- tools/tools.mk
+++ tools/tools.mk
@@ -1,1 +1,2 @@
 include tools/common.mk
+include tools/mediamtx.mk
